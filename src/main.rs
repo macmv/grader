1f8cf3b3df883b899a1af1b3baf0425f21b68c70
Neil Macneale V <neil.macneale.v@gmail.com>
2026-04-11 15:59:17 -0700

    Use the workspace in compile

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,11 @@ mod workspace;
 #[derive(Parser)]
 struct Args {
   #[clap(long, global = true)]
-  course: Option<String>,
+  course:     Option<String>,
+  #[clap(long, global = true)]
+  assignment: Option<String>,
+  #[clap(long, global = true)]
+  dry_run:    bool,
 
   #[clap(subcommand)]
   cmd: Cmd,
@@ -28,11 +32,7 @@ enum Cmd {
   Sections {
     course: String,
   },
-  Download {
-    assignment: Option<String>,
-    #[clap(long)]
-    dry_run:    bool,
-  },
+  Download {},
   Compile {
     /// The .c files to compile
     files: Vec<PathBuf>,
@@ -55,32 +55,47 @@ struct RemoteOutput {
 fn main() {
   let args = Args::parse();
 
+  if let Cmd::Sections { course } = &args.cmd {
+    download::list_sections(&course);
+    return;
+  }
+
+  let workspace = Workspace::new();
+  let course = args
+    .course
+    .as_deref()
+    .map(|name| workspace.course(name))
+    .unwrap_or_else(|| workspace.current_course())
+    .unwrap_or_else(|e| {
+      eprintln!("error: {e}");
+      std::process::exit(1);
+    });
+
+  let assignment = args
+    .assignment
+    .as_deref()
+    .map(|name| course.assignment(name))
+    .unwrap_or_else(|| course.current_assignment())
+    .unwrap_or_else(|e| {
+      eprintln!("error: {e}");
+      std::process::exit(1);
+    });
+
   match args.cmd {
-    Cmd::Sections { course } => download::list_sections(&course),
-    Cmd::Download { assignment, dry_run } => {
-      let workspace = Workspace::new();
-      let course = args
-        .course
-        .as_deref()
-        .map(|name| workspace.course(name))
-        .unwrap_or_else(|| workspace.current_course())
-        .unwrap_or_else(|e| {
-          eprintln!("error: {e}");
-          std::process::exit(1);
-        });
-
-      let assignment = assignment
-        .as_deref()
-        .map(|name| course.assignment(name))
-        .unwrap_or_else(|| course.current_assignment())
-        .unwrap_or_else(|e| {
-          eprintln!("error: {e}");
-          std::process::exit(1);
-        });
-
-      assignment.download_submissions(dry_run);
+    Cmd::Download {} => {
+      assignment.download_submissions(args.dry_run);
     }
-    Cmd::Compile { files } => compile_files(&files),
+    Cmd::Compile { files } => {
+      let files = if files.is_empty() {
+        assignment.path.read_dir().unwrap().map(|e| e.unwrap().path()).collect()
+      } else {
+        files
+      };
+
+      compile_files(&files)
+    }
+
+    Cmd::Sections { .. } => unreachable!(),
   }
 }
 
