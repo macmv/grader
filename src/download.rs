dd77d33b2547b2944157ea599031d595d5190417
Neil Macneale V <neil.macneale.v@gmail.com>
2026-04-10 17:30:30 -0700

    List out all the submissions so far

--- src/download.rs
+++ src/download.rs
@@ -1,5 +1,26 @@
 use crate::workspace::Course;
 
+#[derive(serde::Deserialize)]
+pub struct Submission {
+  pub user_id:     u32,
+  pub score:       Option<f32>,
+  #[serde(default)]
+  pub attachments: Vec<Attachment>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Attachment {
+  pub display_name: String,
+  pub url:          String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct User {
+  pub id:            u32,
+  pub name:          String,
+  pub sortable_name: String,
+}
+
 pub fn list_sections(course: &str) {
   let token = std::fs::read_to_string("../token.txt").unwrap().trim().to_string();
 
@@ -16,6 +37,24 @@ pub fn list_sections(course: &str) {
 }
 
 impl Course {
+  pub fn users(&self) -> Vec<User> {
+    let token = std::fs::read_to_string("../token.txt").unwrap().trim().to_string();
+
+    let res: Vec<User> = ureq::get(format!(
+      "https://wwu.instructure.com/api/v1/sections/{section}/users?per_page=100",
+      section = self.settings.section
+    ))
+    .header("Authorization", &format!("Bearer {token}"))
+    .header("Accept", "application/json")
+    .call()
+    .unwrap()
+    .body_mut()
+    .read_json()
+    .unwrap();
+
+    res
+  }
+
   pub fn download_submissions(&self, assignment: &str) {
     let assignment_id = self
       .settings
@@ -29,8 +68,8 @@ impl Course {
 
     let token = std::fs::read_to_string("../token.txt").unwrap().trim().to_string();
 
-    let res = ureq::get(format!(
-      "https://wwu.instructure.com/api/v1/sections/{section}/assignments/{assignment_id}/submissions",
+    let mut submissions: Vec<Submission> = ureq::get(format!(
+      "https://wwu.instructure.com/api/v1/sections/{section}/assignments/{assignment_id}/submissions?per_page=100",
       section = self.settings.section,
     ))
     .header("Authorization", &format!("Bearer {token}"))
@@ -38,9 +77,31 @@ impl Course {
     .call()
     .unwrap()
     .body_mut()
-    .read_to_string()
+    .read_json()
     .unwrap();
 
-    println!("{res}");
+    println!("{} submission(s)", submissions.len());
+    let users = self.users();
+
+    submissions.sort_by_key(|s| &users.iter().find(|u| u.id == s.user_id).unwrap().sortable_name);
+
+    for s in submissions {
+      let user = users.iter().find(|u| u.id == s.user_id).unwrap();
+
+      if s.attachments.is_empty() {
+        println!("{:<20}: <not submitted>", user.name);
+      } else {
+        let attachment = &s.attachments[0];
+        println!(
+          "{:<20}: {:<20}: {}",
+          user.name,
+          attachment.display_name,
+          match s.score {
+            Some(s) => format!("{s}"),
+            None => format!("<not graded>"),
+          }
+        );
+      }
+    }
   }
 }
